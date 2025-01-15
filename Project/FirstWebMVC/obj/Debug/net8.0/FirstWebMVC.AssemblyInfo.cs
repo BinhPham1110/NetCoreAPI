5bd6c855d1bcaf6307650813b6f2a5e82da43ff2
“BinhPham1110” <binhgold1110@gmail.com>
2025-01-15 18:43:48 +0700

    HTML

--- Project/FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
+++ Project/FirstWebMVC/obj/Debug/net8.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+998ef0666e0252383009a6560e8228c993565679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe113e20739a43abf36d900c5e03b1591d9affe")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
