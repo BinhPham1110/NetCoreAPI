4deb599d022c85038aa946c72537ca9f69ae87b6
“BinhPham1110” <binhgold1110@gmail.com>
2025-03-12 11:04:42 +0700

    BTH 25/2

--- MvcMovie/Models/Person.cs
+++ MvcMovie/Models/Person.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
+    [Table("Personss")]
     public class Person 
     {
         [Key]
--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd6c855d1bcaf6307650813b6f2a5e82da43ff2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c65846431d51204dfac932cf5fff60f01650be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
