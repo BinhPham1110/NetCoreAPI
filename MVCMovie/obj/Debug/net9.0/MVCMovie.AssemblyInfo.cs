94b54674947d638d36df9ba2203195aa0c809d7a
“BinhPham1110” <binhgold1110@gmail.com>
2025-10-30 14:36:18 +0700

    update code

--- MVCMovie/obj/Debug/net9.0/MVCMovie.AssemblyInfo.cs
+++ MVCMovie/obj/Debug/net9.0/MVCMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df58e95ee525f57cfdc33b69c6e637ab37cc4078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfae31b5a791adafd5267a829c21ad86ad96db42")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
